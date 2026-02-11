54e59b1492b21054905d6c503c26d97ae99f58c4
IanLarson43 <iangrantlarson@gmail.com>
2026-02-10 22:09:50 -0600

    clean up data

--- data_scrape.py
+++ data_scrape.py
@@ -17,16 +17,17 @@ for mon in raw_data:
     except(KeyError):
         pass
 
-json.dump(data, file)
+json.dump(data, file, indent = 4)
 
 file = open("data/move_data.json", "w")
 raw_data = requests.get("https://play.pokemonshowdown.com/data/moves.json").json()
 data = []
 
 for move in raw_data:
+    move = move.replace(" ", "")
     data.append(raw_data[move]["name"])
 
-json.dump(data, file)
+json.dump(data, file, indent = 4)
 
 file = open("data/item_data.json", "w")
 raw_data = requests.get("https://play.pokemonshowdown.com/data/items.js").text
@@ -36,9 +37,10 @@ split_raw_data = raw_data.split("name:\"")
 for line in split_raw_data:
     if not line.startswith("exports.BattleItems"):
         split_line = line.split("\"")
-        data.append(split_line[0])
+        item = split_line[0].replace(" ", "")
+        data.append(item)
 
-json.dump(data, file)
+json.dump(data, file, indent = 4)
 
 file = open("data/ability_data.json", "w")
 raw_data = requests.get("https://play.pokemonshowdown.com/data/abilities.js").text
@@ -48,7 +50,8 @@ split_raw_data = raw_data.split("name:\"")
 for line in split_raw_data:
     if not line.startswith("exports.BattleAbilities"):
         split_line = line.split("\"")
-        data.append(split_line[0])
+        ability = split_line[0].replace(" ", "")
+        data.append(ability)
 
-json.dump(data, file)
+json.dump(data, file, indent = 4)
 file.close()
