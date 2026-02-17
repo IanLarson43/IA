8084b950a078dacee3eabdea659ecf87ed319e0b
IanLarson43 <iangrantlarson@gmail.com>
2026-02-16 23:55:04 -0600

    wip dataset creation

--- dataset.py
+++ dataset.py
@@ -1,5 +1,8 @@
 import json
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
+
+dataset_dict = {}
 
 pokemon_file = json.load(open("data/pokemon_data.json"))
 ability_file = json.load(open("data/ability_data.json"))
@@ -21,13 +24,36 @@ ability_encoder.fit(ability_file)
 item_encoder.fit(item_file)
 move_encoder.fit(move_file)
 
-for i in match_file:
-    for j in i:
-        match_dataset = []
-        for mon in j["p1 team"]:
-            match_dataset.append(mon["name"])
-            match_dataset.append(mon["item"])
-            match_dataset.append(mon["ability"])
-            for move in mon["moves"]:
-                match_dataset.append(move)
-            match_dataset.append(mon["tera"])
+for match in range(len(match_file)):
+    match_dataset = []
+    for mon in match_file[match]["p1 team"]:
+        match_dataset.append(mon["name"])
+        match_dataset.append(mon["item"])
+        match_dataset.append(mon["ability"])
+        for move in mon["moves"]:
+            match_dataset.append(move)
+        match_dataset.append(mon["tera"])
+
+    for mon in match_file[match]["p2 team"]:
+        match_dataset.append(mon["name"])
+        match_dataset.append(mon["item"])
+        match_dataset.append(mon["ability"])
+        for move in mon["moves"]:
+            match_dataset.append(move)
+        match_dataset.append(mon["tera"])
+
+    encoded_match_dataset = []
+    for i in match_dataset:
+        if i in pokemon_file:
+            encoded_match_dataset.append(pokemon_encoder.transform([i])[0])
+        elif i in ability_file:
+            encoded_match_dataset.append(ability_encoder.transform([i])[0])
+        elif i in item_file:
+            encoded_match_dataset.append(item_encoder.transform([i])[0])
+        elif i in move_file:
+            encoded_match_dataset.append(move_encoder.transform([i])[0])
+    encoded_match_dataset.append(match_file[match]["winner"])
+
+    dataset_dict[f"match {match}"] = encoded_match_dataset
+
+np.savez("data/dataset.npz", **dataset_dict)
